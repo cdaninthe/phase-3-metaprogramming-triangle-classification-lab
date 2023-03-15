173d2dc656337da37667771122a953399324eced
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-03-15 12:42:13 -0400

    commit tree ea8674f51345aa383396218784bd5e1570c4bc56

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize (length1, length2, length3)
+    @side1 = length1
+    @side2 = length2
+    @side3 = length3
+  end
+
+  def kind
+    if @side1 == 0 || @side2 == 0 || @side3 == 0 || @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side3 + @side2 <= @side1
+      begin
+        raise TriangleError
+        puts error.message    
+      end
+    elsif @side1 == @side2 && @side1 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle"
+    end
+  end
+
 end
